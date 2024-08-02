4ced1ea92a99cab5df690e9313e648583139b675
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-08-02 09:13:51 +0330

    feature(models): refactor

--- src/main/java/com/trivago/casestudy/models/PriceDeserializer.java
+++ src/main/java/com/trivago/casestudy/models/PriceDeserializer.java
@@ -1,4 +1,4 @@
-package com.trivago.casestudy.model;
+package com.trivago.casestudy.models;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
